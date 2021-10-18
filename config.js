99e18d4cd5f8d482ea13fed9af1e4cd847a409d5
demon-killer7 <85291653+demon-killer7@users.noreply.github.com>
2021-10-18 21:49:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introDesc: ' em gì đó ơi! ',
+    introDesc: 'bạn gì đó ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Thích cậu nhất <33',
     btnNo: 'Không, mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao you lại yêu me  <3',
-    btnReply: 'Gửi cho Khải <3',
-    reply: 'Yêu thì yêu mà không yêu thì cũng phải yêu <33333333',
-    mess: ' Ỏ, biết mà 🥰. Yêu hương nhiều nhiều 😘😘',
-    messDesc: 'ok nhớ nhá , cô là của tôi rồi đó .',
+    btnReply: 'Hãy gửi câu trả lời cho Khải <3',
+    reply: 'Thích Khải vì Khải là gu của tôi. Nói chung yêu Khải nhiều lắm <33333333',
+    mess: ' Ỏ, biết mà 🥰. Cảm ơn bạn đã chọn thích tôi 😘😘',
+    messDesc: 'ok nhớ nhá , bây giờ bạn là của tôi rồi đó không chối được đâu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
