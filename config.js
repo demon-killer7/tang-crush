c88fa610df55124195f1f61b474ef785c7042f87
demon-killer7 <85291653+demon-killer7@users.noreply.github.com>
2021-10-18 15:52:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Quỳnh Hương à!',
+    introDesc: ' em gì đó ơi! ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
